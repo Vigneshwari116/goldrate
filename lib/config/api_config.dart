db4f59a8b65b6b6dd4cc806a3bf74ec26017c2ec
Vigneshwari <vigneshwarivcet116@gmail.com>
2026-08-25 12:41:58 +0530

    Point GRATE app to VPS API on port 3002

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -4,8 +4,9 @@ class ApiConfig {
 
   /// Your VPS IP + API port.
   /// Change to https://yourdomain.com/api when you add SSL.
-  static const String baseUrl = 'http://187.127.180.135:3000/api';
+  static const String baseUrl = 'http://187.127.180.135:3002/api';
 
   /// Set true to use VPS PostgreSQL via API instead of local SQLite.
-  static const bool useRemoteApi = false;
+  static const bool useRemoteApi = true;
 }
+
